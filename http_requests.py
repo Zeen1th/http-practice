fb0c900fa23cfe22d34426e50d54592baaa7f712
Basil Alrashedi <121049280+Zeen1th@users.noreply.github.com>
2024-09-08 00:17:09 +0300

    Update http_requests.py

--- http_requests.py
+++ http_requests.py
@@ -1,11 +1,12 @@
 import requests
 
 
-def perform_get_request():
-    """Perform GET request to given URL and return the response"""
+def perform_get_request_with_params():
     url = 'https://httpbin.org/get'
-    response = requests.get(url)
+    params = {'key': 'value'}
+    response = requests.get(url, params=params)
     return response
+
 
 
 def perform_get_request_with_params():
@@ -16,13 +17,14 @@ def perform_get_request_with_params():
 
 
 def perform_post_request():
-    """Perform POST request to given URL sending given data and return the response"""
     url = 'https://httpbin.org/post'
     data = {
         'first_name': 'Guido',
         'last_name': 'van Rossum'
     }
-    pass
+    response = requests.post(url, json=data)
+    return response
+
 
 
 def perform_put_request():
@@ -45,13 +47,14 @@ def perform_patch_request():
 
 
 def perform_delete_request():
-    """Perform DELETE request to given URL and return the response"""
     url = 'https://httpbin.org/delete'
-    pass
+    response = requests.delete(url)
+    return response
+
 
 
 def perform_redirect_request():
-    """Perform a request to a redirect URL and return the Location header that come in the response"""
-    # HINT: you should use the allow_redirects parameter while doing the request
     url = 'https://httpbin.org/redirect/1'
-    pass
+    response = requests.get(url, allow_redirects=False)
+    return response.headers.get('Location')
+
